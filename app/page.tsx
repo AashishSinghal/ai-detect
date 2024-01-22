2dcffca49fcd8a0b78f76fb51d035356928e74f4
AashishSinghal <aashish451singhal@gmail.com>
2024-01-22 11:00:40 +0530

    Added buttons Panel and Side Panel Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
 "use client";
 
 import { ModeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
+import { Slider } from "@/components/ui/slider";
+import beep from "@/utils/audio";
+import {
+  Camera,
+  FlipHorizontal,
+  MoonIcon,
+  PersonStanding,
+  SunIcon,
+  Video,
+  Volume2,
+} from "lucide-react";
 import React, { useRef, useState } from "react";
+import { Rings } from "react-loader-spinner";
 import Webcam from "react-webcam";
+import { toast } from "sonner";
 
 type Props = {};
 
@@ -13,6 +32,9 @@ const HomePage = (props: Props) => {
 
   // States
   const [mirrored, setMirrored] = useState<boolean>(false);
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false);
+  const [volume, setVolume] = useState<number>(0.8);
 
   return (
     <div className="flex h-screen">
@@ -32,25 +54,208 @@ const HomePage = (props: Props) => {
       </div>
       {/* SidePanel */}
       <div className="flex flex-row flex-1">
-        <div className="border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between">
+        <div className="border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between p-2">
           {/* Top */}
           <div className="flex flex-col gap-2">
             <ModeToggle />
-            <Separator />
+            <Button
+              variant={"outline"}
+              size={"icon"}
+              onClick={() => setMirrored((prev) => !prev)}
+            >
+              <FlipHorizontal />
+            </Button>
+            <Separator className="my-2" />
           </div>
           {/* Middle */}
           <div className="flex flex-col gap-2">
-            <Separator />
-            <Separator />
+            <Separator className="my-2" />
+            <Button
+              variant={"outline"}
+              size={"icon"}
+              onClick={userPromptScreenShot}
+            >
+              <Camera />
+            </Button>
+            <Button
+              variant={isRecording ? "destructive" : "outline"}
+              size={"icon"}
+              onClick={userPromptRecord}
+            >
+              <Video />
+            </Button>
+            <Separator className="my-2" />
+            <Button
+              variant={autoRecordEnabled ? "destructive" : "outline"}
+              size={"icon"}
+              onClick={toggleAutoRecord}
+            >
+              {autoRecordEnabled ? (
+                <Rings color="white" height={45} />
+              ) : (
+                <PersonStanding />
+              )}
+            </Button>
           </div>
           {/* Bottom */}
           <div className="flex flex-col gap-2">
-            <Separator />
+            <Separator className="my-2" />
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant={isRecording ? "destructive" : "outline"}
+                  size={"icon"}
+                  onClick={userPromptRecord}
+                >
+                  <Volume2 />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <Slider
+                  max={1}
+                  min={0}
+                  step={0.2}
+                  defaultValue={[volume]}
+                  onValueCommit={(value) => {
+                    setVolume(value[0]);
+                    beep(value[0]);
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
           </div>
+        </div>
+        <div className="h-full flex-1 py-4 px-2 overflow-y-scroll">
+          <RenderFeatureHighlightsSection />
         </div>
       </div>
     </div>
   );
+
+  function userPromptScreenShot() {
+    // Login to take Screenshot on user propmt/click
+  }
+
+  function userPromptRecord() {
+    // Check if recording
+    // then stop recording
+    // save the recording
+    // If not recording
+    // start recording
+  }
+
+  function toggleAutoRecord() {
+    if (autoRecordEnabled) {
+      setAutoRecordEnabled(false);
+      // show toast to notify user
+      toast("Auto Record Disabled");
+    } else {
+      setAutoRecordEnabled(true);
+      // show toast to notify user
+      toast("Auto Record Enabled");
+    }
+  }
+
+  // Inner Components
+  function RenderFeatureHighlightsSection() {
+    return (
+      <div className="text-xs text-muted-foreground">
+        <ul className="space-y-4">
+          <li>
+            <strong>Dark Mode/Sys Theme 🌗</strong>
+            <p>Toggle between dark mode and system theme.</p>
+            <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+              <SunIcon size={14} />
+            </Button>{" "}
+            /{" "}
+            <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+              <MoonIcon size={14} />
+            </Button>
+          </li>
+          <li>
+            <strong>Horizontal Flip ↔️</strong>
+            <p>Adjust horizontal orientation.</p>
+            <Button
+              className="h-6 w-6 my-2"
+              variant={"outline"}
+              size={"icon"}
+              onClick={() => {
+                setMirrored((prev) => !prev);
+              }}
+            >
+              <FlipHorizontal size={14} />
+            </Button>
+          </li>
+          <Separator />
+          <li>
+            <strong>Take Pictures 📸</strong>
+            <p>Capture snapshots at any moment from the video feed.</p>
+            <Button
+              className="h-6 w-6 my-2"
+              variant={"outline"}
+              size={"icon"}
+              onClick={userPromptScreenShot}
+            >
+              <Camera size={14} />
+            </Button>
+          </li>
+          <li>
+            <strong>Manual Video Recording 📽️</strong>
+            <p>Manually record video clips as needed.</p>
+            <Button
+              className="h-6 w-6 my-2"
+              variant={isRecording ? "destructive" : "outline"}
+              size={"icon"}
+              onClick={userPromptRecord}
+            >
+              <Video size={14} />
+            </Button>
+          </li>
+          <Separator />
+          <li>
+            <strong>Enable/Disable Auto Record 🚫</strong>
+            <p>
+              Option to enable/disable automatic video recording whenever
+              required.
+            </p>
+            <Button
+              className="h-6 w-6 my-2"
+              variant={autoRecordEnabled ? "destructive" : "outline"}
+              size={"icon"}
+              onClick={toggleAutoRecord}
+            >
+              {autoRecordEnabled ? (
+                <Rings color="white" height={30} />
+              ) : (
+                <PersonStanding size={14} />
+              )}
+            </Button>
+          </li>
+
+          <li>
+            <strong>Volume Slider 🔊</strong>
+            <p>Adjust the volume level of the notifications.</p>
+          </li>
+          <li>
+            <strong>Camera Feed Highlighting 🎨</strong>
+            <p>
+              Highlights persons in{" "}
+              <span style={{ color: "#FF0F0F" }}>red</span> and other objects in{" "}
+              <span style={{ color: "#00B612" }}>green</span>.
+            </p>
+          </li>
+          <Separator />
+          <li className="space-y-4">
+            <strong>Share your thoughts 💬 </strong>
+            {/* <SocialMediaLinks /> */}
+            <br />
+            <br />
+            <br />
+          </li>
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default HomePage;
